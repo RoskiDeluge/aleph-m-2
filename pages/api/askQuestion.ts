31c0ecc29c2db21c1ddd2129626a3e114370e042
RoskiDeluge <roberto.delgado@gmail.com>
2023-03-10 22:55:45 -1000

    Added askQuestion endpoint and adminDb

--- pages/api/askQuestion.ts
+++ pages/api/askQuestion.ts
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import query from '../../lib/queryApi';
+import admin from 'firebase-admin'
+import { adminDb } from '../../firebaseAdmin';
 
 type Data = {
   answer: string
@@ -21,7 +24,19 @@ export default async function handler(
     }
 
     // ChatGPT query
+    const response = await query(prompt, chatId, model)
 
-    const response = await 
-  res.status(200).json({ name: 'John Doe' })
+    const message: Message = {
+        text: response || "ChatGPT was unable to answer.",
+        createdAt: admin.firestore.Timestamp.now(),
+        user: {
+            _id: 'ChatGPT',
+            name: 'ChatGPT',
+            avatar: 'https://links.papareact.com/89k'
+        }
+    }
+    
+    await adminDb.collection('users').doc(session?.user?.email).collection('chats').doc(chatId).collection('messages').add(message)
+
+  res.status(200).json({ answer: message.text })
 }
